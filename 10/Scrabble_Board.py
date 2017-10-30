efed9fdc8ac2f361290940e5dcb0e40d81c9cb37
Meraz <meraz.mamun88@myhunter.cuny.edu>
2017-10-29 22:35:30 -0400

    10-29-17

--- 10/Scrabble_Board.py
+++ 10/Scrabble_Board.py
@@ -1,3 +1,5 @@
+#I am lost on what to do next. To be more specific, I am having difficulty imagining the next steps for what the code should look like.
+
 TRIPLE_WORD_SCORE = ((0,0), (7, 0), (14,0), (0, 7), (14, 7), (0, 14), (7, 14), (14,14))
 DOUBLE_WORD_SCORE = ((1,1), (2,2), (3,3), (4,4), (1, 13), (2, 12), (3, 11), (4, 10), (13, 1), (12, 2), (11, 3), (10, 4), (13,13), (12, 12), (11,11), (10,10), (7,7))
 TRIPLE_LETTER_SCORE = ((1,5), (1, 9), (5,1), (5,5), (5,9), (5,13), (9,1), (9,5), (9,9), (9,13), (13, 5), (13,9))
